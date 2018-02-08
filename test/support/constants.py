70283de867463847f93ddb58e83acdd1aae03b5d
JaredWright <jared.wright12@gmail.com>
2018-02-08 10:08:36 -0800

    Added mock register XML files for testing shoulder parsers, along with expected values for unit tests

--- test/support/constants.py
+++ test/support/constants.py
@@ -22,8 +22,82 @@
 
 import os
 
+from shoulder.register import Register
+from shoulder.fieldset import Fieldset
+
 TEST_CONSTANTS_PATH = os.path.dirname(os.path.realpath(__file__))
 TEST_TOP_DIR = os.path.join(TEST_CONSTANTS_PATH, os.pardir)
 TEST_SUPPORT_DIR = os.path.join(TEST_TOP_DIR, "support")
 
 TEST_CONFIG_FILE_PATH = os.path.join(TEST_SUPPORT_DIR, "testshoulderconfig.py")
+
+# -----------------------------------------------------------------------------
+# Test XML Register Files and Expected Values
+# -----------------------------------------------------------------------------
+
+TEST_XML_REG32_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_32.xml")
+TEST_XML_REG32 = Register()
+TEST_XML_REG32.name = "MOCK_REGISTER"
+TEST_XML_REG32.long_name = "A mock 32-bit register for testing shoulder"
+TEST_XML_REG32.purpose = "Register does not belong to the aarch64 architecture"
+TEST_XML_REG32.size = 32
+TEST_XML_REG32.is_sysreg = True
+TEST_XML_REG32_FIELDSET = Fieldset(TEST_XML_REG32.size)
+TEST_XML_REG32_FIELDSET.add_field("msb", 31, 31)
+TEST_XML_REG32_FIELDSET.add_field("not_msb_lsb", 30, 1)
+TEST_XML_REG32_FIELDSET.add_field("lsb", 0, 0)
+TEST_XML_REG32.add_fieldset(TEST_XML_REG32_FIELDSET)
+
+TEST_XML_REG64_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_64.xml")
+TEST_XML_REG64 = Register()
+TEST_XML_REG64.name = "MOCK_REGISTER"
+TEST_XML_REG64.long_name = "A mock 64-bit register for testing shoulder"
+TEST_XML_REG64.purpose = "Register does not belong to the aarch64 architecture"
+TEST_XML_REG64.size = 64
+TEST_XML_REG64.is_sysreg = True
+TEST_XML_REG64_FIELDSET = Fieldset(TEST_XML_REG64.size)
+TEST_XML_REG64_FIELDSET.add_field("msb", 63, 63)
+TEST_XML_REG64_FIELDSET.add_field("not_msb_lsb", 62, 1)
+TEST_XML_REG64_FIELDSET.add_field("lsb", 0, 0)
+TEST_XML_REG64.add_fieldset(TEST_XML_REG64_FIELDSET)
+
+TEST_XML_MULTIPLE_FIELDSETS_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_multiple_fieldsets.xml")
+TEST_XML_MULTIPLE_FIELDSETS = Register()
+TEST_XML_MULTIPLE_FIELDSETS.name = "MOCK_REGISTER_MULTIPLE_FIELDSETS"
+TEST_XML_MULTIPLE_FIELDSETS.long_name = "A mock 64-bit register with multiple fields"
+TEST_XML_MULTIPLE_FIELDSETS.purpose = "Register does not belong to the aarch64 architecture"
+TEST_XML_MULTIPLE_FIELDSETS.size = 64
+TEST_XML_MULTIPLE_FIELDSETS.is_sysreg = True
+TEST_XML_MULTIPLE_FIELDSETS_FIELDSET = Fieldset(TEST_XML_MULTIPLE_FIELDSETS.size)
+TEST_XML_MULTIPLE_FIELDSETS_FIELDSET.add_field("msb", 63, 63)
+TEST_XML_MULTIPLE_FIELDSETS_FIELDSET.add_field("not_msb_lsb", 62, 1)
+TEST_XML_MULTIPLE_FIELDSETS_FIELDSET.add_field("lsb", 0, 0)
+TEST_XML_MULTIPLE_FIELDSETS.add_fieldset(TEST_XML_REG64_FIELDSET)
+TEST_XML_MULTIPLE_FIELDSETS_FIELDSET = Fieldset(TEST_XML_MULTIPLE_FIELDSETS.size)
+TEST_XML_MULTIPLE_FIELDSETS_FIELDSET.add_field("upper_half", 63, 22)
+TEST_XML_MULTIPLE_FIELDSETS_FIELDSET.add_field("lower_half", 21, 0)
+TEST_XML_MULTIPLE_FIELDSETS.add_fieldset(TEST_XML_REG64_FIELDSET)
+
+TEST_XML_NAME_ONLY_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_name_only.xml")
+TEST_XML_NAME_ONLY = Register()
+TEST_XML_NAME_ONLY.name = "NAME_ONLY_REGISTER"
+TEST_XML_NAME_ONLY.long_name = None
+TEST_XML_NAME_ONLY.purpose = None
+TEST_XML_NAME_ONLY.size = None
+TEST_XML_NAME_ONLY.is_sysreg = True
+TEST_XML_NAME_ONLY_FIELDSET = []
+
+TEST_XML_INVALID_FIELD_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_invalid_field.xml")
+TEST_XML_INVALID_FIELD = Register()
+TEST_XML_INVALID_FIELD.name = "MOCK_REGISTER_INVALID_FIELD"
+TEST_XML_INVALID_FIELD.long_name = "A mock 64-bit register with an invalid field"
+TEST_XML_INVALID_FIELD.purpose = None
+TEST_XML_INVALID_FIELD.size = 64
+TEST_XML_INVALID_FIELD.is_sysreg = True
+TEST_XML_INVALID_FIELD_FIELDSET = []
+
+TEST_XML_MULTIPLE_PURPOSE_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_multiple_purpose.xml")
+
+TEST_XML_NO_ATTRIBUTES_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_no_attributes.xml")
+
+TEST_XML_EMPTY_PATH = os.path.join(TEST_SUPPORT_DIR, "mock_register_empty.xml")
