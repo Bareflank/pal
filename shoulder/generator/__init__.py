78d4a17542f8027ed57eb60512a2bf345f464a67
JaredWright <jared.wright12@gmail.com>
2018-04-26 17:56:57 -0600

    Added top-level generator module interface

--- shoulder/generator/__init__.py
+++ shoulder/generator/__init__.py
@@ -19,3 +19,36 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+import os
+import pkgutil
+
+from shoulder.logger import logger
+
+pkg_dir = os.path.dirname(__file__)
+for (module_loader, name, ispkg) in pkgutil.iter_modules([pkg_dir]):
+    pkgutil.importlib.import_module('.' + name, __package__)
+
+all_generators = [cls for cls in abstract_generator.AbstractGenerator.__subclasses__()]
+
+# -----------------------------------------------------------------------------
+# Module interface
+# -----------------------------------------------------------------------------
+
+# Usage:
+#
+# from shoulder.generator import *
+# generate_all()
+
+def generate_all(objects, outdir):
+    logger.info("Generating outputs")
+    for g_cls in all_generators:
+        sub_outdir = os.path.abspath(os.path.join(outdir, "cxx"))
+        if not os.path.exists(sub_outdir):
+                os.makedirs(sub_outdir)
+        outfile = os.path.abspath(os.path.join(sub_outdir, "shoulder.h"))
+
+        g = g_cls()
+        g.generate(objects, outfile)
+
+    logger.info("Generation complete")
