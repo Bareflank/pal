b3edf4c4f71e2c3c3f6716240e8d8b548739228f
JaredWright <jared.wright12@gmail.com>
2019-11-05 00:19:36 +0000

    Add access mechanism writer definitions
    1) Add access mechanism writer for c++ unit testing
    1) Add access mechanism writer for GAS assembler using intel syntax
    1) Add access mechanism writer for GAS assembler using att syntax
    1) Add empty/null access mechanism writer
    
    Signed-off-by: JaredWright <jared.wright12@gmail.com>

---
+++ shoulder/writer/access_mechanism/gas_x86_64_intel_syntax.py
@@ -0,0 +1,116 @@
+from shoulder.writer.access_mechanism.access_mechanism \
+        import AccessMechanismWriter
+from shoulder.logger import logger
+
+
+class GasX86_64IntelSyntaxAccessMechanismWriter(AccessMechanismWriter):
+
+    def declare_access_mechanism_dependencies(self, outfile, register):
+        pass
+
+    def call_readable_access_mechanism(self, outfile, register,
+                                       access_mechanism, result):
+        if access_mechanism.name == "mov_read":
+            self._call_mov_read_access_mechanism(outfile, register,
+                                                 access_mechanism, result)
+        elif access_mechanism.name == "cpuid":
+            self._call_cpuid_access_mechanism(outfile, register,
+                                              access_mechanism, result)
+        elif access_mechanism.name == "rdmsr":
+            self._call_rdmsr_access_mechanism(outfile, register,
+                                              access_mechanism, result)
+        else:
+            msg = "Access mechnism {am} is not supported using "
+            msg += "Intel x86_64 gas intel assembler syntax".format(
+                am=access_mechanism.name
+            )
+            logger.warn(msg)
+
+    def call_writable_access_mechanism(self, outfile, register,
+                                       access_mechanism, value):
+        if access_mechanism.name == "mov_write":
+            self._call_mov_write_access_mechanism(outfile, register,
+                                                  access_mechanism, value)
+        elif access_mechanism.name == "wrmsr":
+            self._call_wrmsr_access_mechanism(outfile, register,
+                                              access_mechanism, value)
+        else:
+            msg = "Access mechnism {am} is not supported using "
+            msg += "Intel x86_64 gas intel assembler syntax".format(
+                am=access_mechanism.name
+            )
+            logger.warn(msg)
+
+    def _call_mov_read_access_mechanism(self, outfile, register,
+                                        access_mechanism, result):
+        self._write_inline_assembly(outfile, [
+                "mov %[v], " + access_mechanism.source_mnemonic
+            ],
+            outputs='[v] "=r"(' + str(result) + ')'
+        )
+
+    def _call_cpuid_access_mechanism(self, outfile, register, access_mechanism,
+                                     result):
+        if register.is_indexed:
+            subleaf_mnemonic = "%[subleaf]"
+            subleaf_input = '[subleaf] "r"(index)'
+        else:
+            subleaf_mnemonic = "0"
+            subleaf_input = ""
+
+        self._write_inline_assembly(outfile, [
+                "mov eax, " + str(hex(access_mechanism.leaf)),
+                "mov ecx, " + subleaf_mnemonic,
+                "cpuid",
+                "mov %[out], " + access_mechanism.output
+            ],
+            outputs='[out] "=r"(' + result + ')',
+            inputs=subleaf_input,
+            clobbers='"eax", "ebx", "ecx", "edx"'
+        )
+
+    def _call_rdmsr_access_mechanism(self, outfile, register, access_mechanism,
+                                     result):
+        self._write_inline_assembly(outfile, [
+                "mov rcx, " + str(hex(access_mechanism.address)),
+                "rdmsr",
+                "shl rdx, 32",
+                "or rax, rdx",
+                "mov %[v], rax",
+            ],
+            outputs='[v] "=r"(' + result + ')',
+            clobbers='"rax", "rcx", "rdx"'
+        )
+
+    def _call_mov_write_access_mechanism(self, outfile, register,
+                                         access_mechanism, value):
+        self._write_inline_assembly(outfile, [
+                "mov " + access_mechanism.destination_mnemonic + ", %[v]",
+            ],
+            inputs='[v] "r"(' + value + ')'
+        )
+
+    def _call_wrmsr_access_mechanism(self, outfile, register, access_mechanism,
+                                     value):
+        self._write_inline_assembly(outfile, [
+                "mov rcx, " + str(hex(access_mechanism.address)),
+                "mov rax, %[v]",
+                "mov rdx, %[v]",
+                "shr rdx, 32",
+                "wrmsr",
+            ],
+            inputs='[v] "r"(' + value + ')',
+            clobbers='"rax", "rcx", "rdx"'
+        )
+
+    def _write_inline_assembly(self, outfile, statements, outputs="",
+                               inputs="", clobbers=""):
+        outfile.write("__asm__ __volatile__(\n")
+        for statement in statements:
+            outfile.write("    \"" + str(statement) + ";\"\n")
+
+        outfile.write("    : " + str(outputs) + "\n")
+        outfile.write("    : " + str(inputs) + "\n")
+        outfile.write("    : " + str(clobbers) + "\n")
+        outfile.write(");")
+        self.write_newline(outfile)
