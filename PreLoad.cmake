ea795bd545d01bcf6a0268e47c3a3882ff602cdf
Nick Rosbrook <rosbrookn@ainfosec.com>
2020-06-22 16:51:27 -0700

    remove restrictive cmake files
    PreLoad.cmake would only allow ninja to be used as the generator on
    Windows, but we want to add support for Visual Studio with libpal.
    Remove similar restrictions from macros/pal_print_usage.cmake.
    
    Signed-off-by: Nick Rosbrook <rosbrookn@ainfosec.com>

--- PreLoad.cmake
+++
@@ -1,5 +0,0 @@
-# Ninja is the only supported CMake generator on Windows
-if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL Windows)
-    message(STATUS "Using CMake Generator: Ninja")
-    set(CMAKE_GENERATOR "Ninja" CACHE INTERNAL "")
-endif()
