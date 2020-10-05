a39dd77c2ac2acd244ec59c732995ff01f74bfcd
JaredWright <jared.wright12@gmail.com>
2020-10-05 22:25:39 +0000

    Update tests for compatibilty on default Ubuntu 18.04 compilers
    Signed-off-by: JaredWright <jared.wright12@gmail.com>

--- test/c++11/armv8-a/aarch64/main.cpp
+++ test/c++11/armv8-a/aarch64/main.cpp
@@ -13,4 +13,13 @@ int putchar(int c)
 int __printf_chk(int flag, const char * format, ...)
 { return 0; }
 
+int printf ( const char * format, ... )
+{ return 0; }
+
+void __stack_chk_guard(void)
+{}
+
+void __stack_chk_fail(void)
+{}
+
 }
