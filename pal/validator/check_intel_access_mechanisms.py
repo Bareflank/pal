6859c02d256a8ddb6b187118ae9845d128028652
girouxj <girouxj@ainfosec.com>
2020-10-19 12:16:57 -0400

    fixing layout
    Signed-off-by: girouxj <girouxj@ainfosec.com>

--- pal/validator/check_intel_access_mechanisms.py
+++ pal/validator/check_intel_access_mechanisms.py
@@ -1,30 +1,19 @@
-import yaml
 from pal.logger import logger
-from os import listdir
-from os.path import isfile, join
 
-def check_intel_access_mechanisms_group(indir):
-    # Get all file names within the indir (Control Reg for Intel)
-    intel_control_reg_files = [f for f in listdir(indir) if isfile(join(indir, f))]
-
-    for file in intel_control_reg_files:
-        contents = read_contents(file, indir)
-        check_source_mnemonic(contents, file)
-        check_access_mechanisms_names(contents, file)
-
-def read_contents(fileName, directory):
-    with open(directory + "/" + fileName) as file:
-        return yaml.safe_load(file)
+def check_intel_access_mechanisms_group(indir, contents):
+    for file_index in range(len(contents)):
+        check_access_mechanisms_functions(contents[file_index], indir)
+        check_access_mechanisms_names(contents[file_index], indir)
 
 # Used to read in control register for intel architecture
-def check_source_mnemonic(contents, file):
+def check_access_mechanisms_functions(contents, file):
     # go through each file and check if the source_mnemonic matches
-    logger.info("Validating source mnemonic in file: intel/" + file)
-    name = contents[0]["name"]
+    name = contents.name
+    logger.info("Validating source mnemonic in file: intel/" + name + ".yml")
     # xcr0 in intel does not contain source_mnemonic or destination_mnemonic
     if name != "xcr0":
-        read_source_mnemonic = contents[0]["access_mechanisms"][0]["source_mnemonic"]
-        write_destination_mnemonic = contents[0]["access_mechanisms"][1]["destination_mnemonic"]
+        read_source_mnemonic = contents.access_mechanisms["mov_read"][0].source_mnemonic
+        write_destination_mnemonic = contents.access_mechanisms["mov_write"][0].destination_mnemonic
 
         if name != read_source_mnemonic:
             logger.error("Name does not match source_mnemonic")
@@ -35,18 +24,23 @@ def check_source_mnemonic(contents, file):
         if read_source_mnemonic != write_destination_mnemonic:
             logger.error("Read source mnemonic does not match write source mnemonic")
 
+
 # make sure access mechanisms name matches mov_read/move_write, or, xsetbv/xgetbv
 def check_access_mechanisms_names(contents, file):
-    logger.info("Validating access mechanisms names in file: intel/" + file)
-    name = contents[0]["name"]
-    
-    if name != "xcr0":
-        if contents[0]["access_mechanisms"][0]["name"] != "mov_read":
+    name = contents.name
+    logger.info("Validating access mechanisms names in file: intel/" + name + ".yml")
+
+    if name == "xcr0":
+        access_mechanisms_read_name = contents.access_mechanisms["xgetbv"][0].name
+        access_mechanisms_write_name = contents.access_mechanisms["xsetbv"][0].name
+        if access_mechanisms_read_name != "xgetbv":
             logger.error(file + " Access mechanism name [0] does not match 'mov_read'")
-        if contents[0]["access_mechanisms"][1]["name"] != "mov_write":
+        if access_mechanisms_write_name != "xsetbv":
             logger.error(file + " Access mechanism name [1] does not match 'mov_write'")
-    elif name == "xcr0":
-        if contents[0]["access_mechanisms"][0]["name"] != "xgetbv":
+    else:
+        access_mechanisms_read_name = contents.access_mechanisms["mov_read"][0].name
+        access_mechanisms_write_name = contents.access_mechanisms["mov_write"][0].name
+        if access_mechanisms_read_name != "mov_read":
             logger.error(file + " Access mechanism name [0] does not match 'mov_read'")
-        if contents[0]["access_mechanisms"][1]["name"] != "xsetbv":
+        if access_mechanisms_write_name != "mov_write":
             logger.error(file + " Access mechanism name [1] does not match 'mov_write'")
