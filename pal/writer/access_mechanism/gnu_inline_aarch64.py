83d2a5fad6375cece5b9776329f747a602728fc0
awsjared <awsjared@amazon.com>
2021-08-06 15:42:12 -0700

    Combine GNU inline assmbly access mechanisms
    1) Update access mechanisms configs to generic "gnu_inline"
    2) Combine access mechanism writers for gnu inline syntaxes (ATT/Intel)
    3) Update README and tests
    
    Signed-off-by: Jared Wright <awsjared@amazon.com>

--- pal/writer/access_mechanism/gnu_inline_aarch64.py
+++ pal/writer/access_mechanism/gnu_inline_aarch64.py
@@ -4,7 +4,7 @@ from pal.writer.access_mechanism.access_mechanism \
     import AccessMechanismWriter
 
 
-class GasAarch64AccessMechanismWriter(AccessMechanismWriter):
+class GnuInlineAarch64AccessMechanismWriter(AccessMechanismWriter):
 
     def declare_access_mechanism_dependencies(self, outfile, register,
                                        access_mechanism):
