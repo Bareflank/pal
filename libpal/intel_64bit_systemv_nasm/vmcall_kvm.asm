015227a9a279c7b4936e3e6759ae636ee422a977
awsjared <awsjared@amazon.com>
2021-08-16 15:24:45 -0700

    Fix rbx register clobber in intel_64bit_linux_nasm vmcall_kvm
    Signed-off-by: Jared Wright <awsjared@amazon.com>

--- libpal/intel_64bit_systemv_nasm/vmcall_kvm.asm
+++ libpal/intel_64bit_systemv_nasm/vmcall_kvm.asm
@@ -5,9 +5,13 @@ section .text
 
 global pal_execute_vmcall_kvm
 pal_execute_vmcall_kvm :
+    push rbx
+
     mov rax, rdi
     mov rbx, rsi
     xchg rcx, rdx
     mov rsi, r8
     vmcall
+
+    pop rbx
     ret
