cf29d5fbc449d28afb348d9c4e317b65cdc6e61d
Vikram Narayanan <vikram186@gmail.com>
2021-09-22 02:20:55 -0700

    cmake: Fail if python modules are missing

--- cmake/macros/macros.cmake
+++ cmake/macros/macros.cmake
@@ -1,5 +1,6 @@
 include(${CMAKE_CURRENT_LIST_DIR}/pal_add_config.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pal_add_compilation_test.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/pal_find_python_module.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pal_print_banner.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pal_print_usage.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pal_run_generator.cmake)
