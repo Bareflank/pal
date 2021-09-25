84e5139cfd41a96a35c0ec878889f01813704c80
Zhaofeng Li <hello@zhaofeng.li>
2021-09-24 23:12:38 -0700

    Add documentation generation API

--- pal/writer/comment/comment.py
+++ pal/writer/comment/comment.py
@@ -7,3 +7,11 @@ class CommentWriter(abc.ABC):
     @abc.abstractmethod
     def declare_comment(self, outfile: TextIO, comment: str, wrap: int) -> None:
         pass
+
+    @abc.abstractmethod
+    def declare_file_documentation(self, outfile: TextIO, summary: str, wrap: int, details: str="") -> None:
+        pass
+
+    @abc.abstractmethod
+    def declare_item_documentation(self, outfile: TextIO, summary: str, wrap: int, details: str="") -> None:
+        pass
