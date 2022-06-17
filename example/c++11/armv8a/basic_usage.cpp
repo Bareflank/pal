8a56626f8c4bc797656eab0b3cfb393772b134b7
Jared Wright <awsjared@amazon.com>
2022-06-17 22:49:54 +0000

    Add empty example projects for C and C++ on ARMv8a
    Signed-off-by: Jared Wright <awsjared@amazon.com>

---
+++ example/c++11/armv8a/basic_usage.cpp
@@ -0,0 +1,9 @@
+// TODO: Import generated PAL headers here
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "TODO: Implement an example that usees PAL to read SCTLR_EL1" << std::endl;
+    std::cout << "NOTE: Don't forget to load the devpal driver before running the example!" << std::endl;
+}
---
+++ example/c/armv8a/basic_usage.c
@@ -0,0 +1,9 @@
+// TODO: Import generated PAL headers here
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("TODO: Implement an example that usees PAL to read SCTLR_EL1\n");
+    printf("NOTE: Don't forget to load the devpal driver before running the example!\n");
+}
