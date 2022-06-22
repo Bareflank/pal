e3ae8ab1f8478ab8c37b275e082555e8fc219405
Davit Gabrielyan <dgg@amazon.com>
2022-06-22 01:08:52 +0000

    Changes to SCTRL_EL1 module and example program

--- devpal/linux/armv8a/aarch64/sysl.c
+++ devpal/linux/armv8a/aarch64/sysl.c
@@ -1,8 +1,10 @@
 #include <linux/uaccess.h>
 #include "devpal_abi_armv8a_aarch64.h"
+#include <linux/printk.h>
 
 long handle_devpal_ioctl_sysl(struct sysl_operands * user_ops)
 {
+    //Init of variables/operands 
     uint8_t op1 = 0;
     uint8_t crn = 0;
     uint8_t crm = 0;
@@ -18,10 +20,10 @@ long handle_devpal_ioctl_sysl(struct sysl_operands * user_ops)
     crm = kern_ops.in.crm;
     op2 = kern_ops.in.op2;
 
-    // TODO: Implement Me!
-    //  - take the fields that make up the encoding for the A64 SYS instruction
-    //  - calculate the instruction encoding
-    //  - map the encoding to a function implemented within the driver
+    //inline assembly for mrs
+    __asm__ volatile("mrs %0, s3_0_c1_c0_0"
+                   : "=r"(value));
+
 
     kern_ops.out.value = value;
     if (copy_to_user(user_ops, &kern_ops, sizeof(struct sysl_operands)))
--- example/c/armv8a/basic_usage.c
+++ example/c/armv8a/basic_usage.c
@@ -1,9 +1,14 @@
 // TODO: Import generated PAL headers here
 
 #include <stdio.h>
+#include <pal/aarch64/sctlr_el1.h>
 
 int main(void)
 {
-    printf("TODO: Implement an example that usees PAL to read SCTLR_EL1\n");
     printf("NOTE: Don't forget to load the devpal driver before running the example!\n");
+
+    uint64_t val =  pal_get_sctlr_el1();
+    pal_print_sctlr_el1_from_value(printf, val);
+    printf("SCTLR_EL1 decimal:: %ld\n", val);
+    printf("SCTLR_EL1 hex:: %lx\n", val);
 }
