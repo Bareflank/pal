720a9b8b4ef79a7f7f07fc4f0d620c0074c8b277
Davit Gabrielyan <dgg@amazon.com>
2022-06-23 01:03:03 +0000

    Changes to enable setting a value to sctlr_el1

--- example/c/armv8a/basic_usage.c
+++ example/c/armv8a/basic_usage.c
@@ -9,6 +9,17 @@ int main(void)
 
     uint64_t val =  pal_get_sctlr_el1();
     pal_print_sctlr_el1_from_value(printf, val);
-    printf("SCTLR_EL1 decimal:: %ld\n", val);
-    printf("SCTLR_EL1 hex:: %lx\n", val);
+    printf("SCTLR_EL1 hex: %lx\n", val);
+
+    // Change a specific bit in sctlr_el1
+    /*
+    uint64_t tmp = 0x8;
+    val ^= tmp;
+    printf("After xor: %lx\n", val);
+    pal_set_sctlr_el1(val);
+
+    val = pal_get_sctlr_el1();
+    printf("SCTLR_EL1 hex: %lx\n", val);
+    pal_print_sctlr_el1_from_value(printf, val);
+    */
 }
