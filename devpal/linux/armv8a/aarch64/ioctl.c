09de9f36c1dc4b4effbdae89cf845587c789818a
Davit Gabrielyan <dgg@amazon.com>
2022-07-29 20:11:29 -0700

    Added read/write functionality for ARM kernel virtual address space

--- devpal/linux/armv8a/aarch64/ioctl.c
+++ devpal/linux/armv8a/aarch64/ioctl.c
@@ -10,12 +10,21 @@
 
 long handle_devpal_ioctl_sys(struct sys_operands * user_ops);
 long handle_devpal_ioctl_sysl(struct sysl_operands * user_ops);
-long handle_devpal_ioctl_smc(struct smc_operands *user_ops);
-long handle_devpal_ioctl_hvc(struct hvc_operands *user_ops);
+long handle_devpal_ioctl_smc(struct smc_operands * user_ops);
+long handle_devpal_ioctl_hvc(struct hvc_operands * user_ops);
+long handle_devpal_ioctl_read_mem8(struct read_mem8_operands *user_ops);
+long handle_devpal_ioctl_read_mem16(struct read_mem16_operands *user_ops);
+long handle_devpal_ioctl_read_mem32(struct read_mem32_operands *user_ops);
+long handle_devpal_ioctl_read_mem64(struct read_mem64_operands *user_ops);
+long handle_devpal_ioctl_write_mem8(struct write_mem8_operands *user_ops);
+long handle_devpal_ioctl_write_mem16(struct write_mem16_operands *user_ops);
+long handle_devpal_ioctl_write_mem32(struct write_mem32_operands *user_ops);
+long handle_devpal_ioctl_write_mem64(struct write_mem64_operands *user_ops);
 
 long handle_devpal_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
     printk("[devpal] ioctl: %u\n", cmd);
+
     switch (cmd) {
         case DEVPAL_EXECUTE_SYS:
             return handle_devpal_ioctl_sys((struct sys_operands *)arg);
@@ -28,6 +37,30 @@ long handle_devpal_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
         case DEVPAL_EXECUTE_HVC:
             return handle_devpal_ioctl_hvc((struct hvc_operands *)arg);
+
+        case DEVPAL_EXECUTE_READ_MEM8:
+            return handle_devpal_ioctl_read_mem8((struct read_mem8_operands *)arg);
+
+        case DEVPAL_EXECUTE_READ_MEM16:
+            return handle_devpal_ioctl_read_mem16((struct read_mem16_operands *)arg);
+
+        case DEVPAL_EXECUTE_READ_MEM32:
+            return handle_devpal_ioctl_read_mem32((struct read_mem32_operands *)arg);
+
+        case DEVPAL_EXECUTE_READ_MEM64:
+            return handle_devpal_ioctl_read_mem64((struct read_mem64_operands *)arg);
+
+        case DEVPAL_EXECUTE_WRITE_MEM8:
+            return handle_devpal_ioctl_write_mem8((struct write_mem8_operands *)arg);
+
+        case DEVPAL_EXECUTE_WRITE_MEM16:
+            return handle_devpal_ioctl_write_mem16((struct write_mem16_operands *)arg);
+
+        case DEVPAL_EXECUTE_WRITE_MEM32:
+            return handle_devpal_ioctl_write_mem32((struct write_mem32_operands *)arg);
+
+        case DEVPAL_EXECUTE_WRITE_MEM64:
+            return handle_devpal_ioctl_write_mem64((struct write_mem64_operands *)arg);
 
         default:
             return -EINVAL;
--- example/c/armv8a/basic_usage.c
+++ example/c/armv8a/basic_usage.c
@@ -6,10 +6,16 @@
 #include <pal/aarch64/elr_el1.h>
 #include <pal/aarch64/spsr_el1.h>
 #include <pal/aarch64/ttbr0_el1.h>
+#include <pal/aarch64/ttbr1_el1.h>
+#include <pal/aarch64/vttbr_el2.h>
 #include <pal/aarch64/vbar_el1.h>
 #include <pal/aarch64/dbgauthstatus_el1.h>
 #include <pal/aarch64/dbgbcr0_el1.h>
+#include <pal/aarch64/par_el1.h>
+#include <pal/aarch64/pmmir_el1.h>
 
+#include "pal/a64/read_mem.h"
+#include "pal/a64/write_mem.h"
 #include "pal/a64/smc.h"
 #include "pal/a64/hvc.h"
 
@@ -22,31 +28,50 @@ int main(void)
 {
     printf("NOTE: Don't forget to load the devpal driver before running the example!\n");
 
-    uint64_t val = pal_get_esr_el1();
-    pal_print_esr_el1_from_value(printf, val);
-    printf("esr_el1 hex: %lx\n", val);
+    uint64_t val = 0;
+    uintptr_t tmp = 0;
 
-    val =  pal_get_sctlr_el1();
-    pal_print_sctlr_el1_from_value(printf, val);
-    printf("SCTLR_EL1 hex: %lx\n", val);
+    // val = pal_get_esr_el1();
+    // pal_print_esr_el1_from_value(printf, val);
 
-    val = pal_get_elr_el1();
-    pal_print_elr_el1_from_value(printf, val);
+    // val =  pal_get_sctlr_el1();
+    // pal_print_sctlr_el1_from_value(printf, val);
 
-    val = pal_get_spsr_el1();
-    pal_print_spsr_el1_fieldset_2_from_value(printf, val);
+    // val = pal_get_elr_el1();
+    // pal_print_elr_el1_from_value(printf, val);
+
+    // val = pal_get_spsr_el1();
+    // pal_print_spsr_el1_fieldset_2_from_value(printf, val);
 
     val = pal_get_ttbr0_el1();
     pal_print_ttbr0_el1_from_value(printf, val);
 
-    val = pal_get_vbar_el1();
-    pal_print_vbar_el1_from_value(printf, val);
+    val = pal_get_ttbr1_el1();
+    pal_print_ttbr1_el1_from_value(printf, val);
+    tmp = pal_get_ttbr1_el1_baddr();
+    printf("OOO: %p\n", (void *)tmp);
 
-    val = pal_get_dbgauthstatus_el1();
-    pal_print_dbgauthstatus_el1_from_value(printf, val);
+    val = pal_get_par_el1();
+    pal_print_par_el1_fieldset_1_sh_from_value(printf, val);
+    pal_print_par_el1_fieldset_2_s_from_value(printf, val);
 
-    val = pal_get_dbgbcr0_el1();
-    pal_print_dbgbcr0_el1_from_value(printf, val);
+    val = pal_get_pmmir_el1();
+    pal_print_pmmir_el1_from_value(printf, val);
+
+    // val = pal_get_vttbr_el2();
+    // pal_print_vttbr_el2_from_value(printf, val);
+    // //tmp = pal_get_ttbr1_el1_baddr();
+    // //printf("OOO: %p\n", (void *)tmp);
+    // printf("OOO: %lx\n", val);
+
+    // val = pal_get_vbar_el1();
+    // pal_print_vbar_el1_from_value(printf, val);
+
+    // val = pal_get_dbgauthstatus_el1();
+    // pal_print_dbgauthstatus_el1_from_value(printf, val);
+
+    // val = pal_get_dbgbcr0_el1();
+    // pal_print_dbgbcr0_el1_from_value(printf, val);
 
     // Set functions from here on DANGER ZONE. Real danger of crushing the system
 
@@ -54,33 +79,33 @@ int main(void)
     
 
     // Change contents of dbgbcr0_el1 register
-    val = pal_get_dbgbcr0_el1();
-    pal_print_dbgbcr0_el1_from_value(printf, val);
+    // val = pal_get_dbgbcr0_el1();
+    // pal_print_dbgbcr0_el1_from_value(printf, val);
 
-    uint64_t tmp = 0x6;
+    //uint64_t tmp = 0x6;
     //printf("After xor: %lx\n", val);
-    pal_set_dbgbcr0_el1(tmp);
+    //pal_set_dbgbcr0_el1(tmp);
 
-    val = pal_get_dbgbcr0_el1();
-    printf("dbgbcr0_el1 hex: %lx\n", val);
-    pal_print_dbgbcr0_el1_from_value(printf, val);
+    // val = pal_get_dbgbcr0_el1();
+    // printf("dbgbcr0_el1 hex: %lx\n", val);
+    // pal_print_dbgbcr0_el1_from_value(printf, val);
 
-    val = pal_get_elr_el1();
-    pal_print_elr_el1_from_value(printf, val);
+    // val = pal_get_elr_el1();
+    // pal_print_elr_el1_from_value(printf, val);
 
-    printf("Here func addr: %p\n", test_func);
-    pal_set_elr_el1((uint64_t)test_func);
+    // printf("Here func addr: %p\n", test_func);
+    // pal_set_elr_el1((uint64_t)test_func);
 
-    val = pal_get_elr_el1();
-    printf("elr hex: %lx\n", val);
-    pal_print_elr_el1_from_value(printf, val);
+    // val = pal_get_elr_el1();
+    // printf("elr hex: %lx\n", val);
+    // pal_print_elr_el1_from_value(printf, val);
 
     printf("=====================================\n");
     struct hvc_operands args = {0};
-    args.in.W0 = 0x84000000;
-    //args.in.X1 = 0xdeadbeef;
-    // args.in.X2 = 0xdeadbeef;
-    // args.in.X3 = 0xdeadbeef;
+    args.in.W0 = 0x84000132;
+    args.in.X1 = 0xdeadbeef;
+    args.in.X2 = 0x20;
+    args.in.X3 = 0x4;
     args = pal_execute_hvc(&args);
 
     printf("Here X0: %lx\n", args.out.X0);
@@ -98,4 +123,9 @@ int main(void)
     printf("Here X12: %lx\n", args.out.X12);
     printf("Here X13: %lx\n", args.out.X13);
     printf("Here X14: %lx\n", args.out.X14);
+
+    uintptr_t pp = 0x14fc50b7;
+    printf("Address example (userspace) program is trying to read: %p\n", (void *) pp);
+    val = pal_execute_read_mem64(pp);
+    printf("Here is the value: %lx\n", val);
 }
